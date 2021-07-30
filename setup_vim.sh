943c66eb7b0983e2b3dca23865b8868dbd9aa0be
Simun Strukan <simun.strukan@gmail.com>
2021-07-30 13:00:33 +0200

    Initial commit

---
+++ setup_vim.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### configure terminal
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10ka
+
+# Update .zshrc file with pk10 theme
+
+
+### Install and configure vim ###
+
+#Install vim plugins and misc goodies
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+#Install SpaceGray colorscheme
+git clone git://github.com/ajh17/Spacegray.vim ~/.vim/pack/vendor/start/Spacegray
+
+#Install Iceberg color scheme
+git clone git://github.com/cocopon/iceberg.vim ~/.vim/pack/vendor/start/Iceberg
+
+#Create custom vim config
+touch ~/.vim_runtime/my_configs.vim
+
+#Add custom modifications
+echo ":set number" >> my_configs.vim
+echo ":syntax on" >> my_config.vim
+echo ":set termguicolors" >> my_config.vim
+echo ":colorscheme iceberg" >> my_config.vim
+echo ":syntax on" >> my_config.vim
+echo ":hi LineNr ctermbg=236 ctermfg=3 guibg=#1e2132 guifg=#444b71" >> my_config.vim
